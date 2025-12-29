45febf3ce0d331d4b9afc4117500b27de4d7725f
SamrathsinhRathod <Samrathsinhrathod031@gmail.com>
2025-12-29 11:37:27 +0530

    Add files via upload

---
+++ samrathsinh11111.py
@@ -0,0 +1,26 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=9
+print(x)
+9
+y=5
+print(-y)
+-5
+x=9
+y=3
+print(x>y)
+True
+print(x==y)
+False
+a=5
+b=9
+print(a&b)
+1
+print(a|b)
+13
+x=6
+y=9
+print(x is y)
+False
+print(x is not y)
+True
