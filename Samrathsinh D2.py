6968e9bda649b95f996befe887e23758bec785c7
SamrathsinhRathod <Samrathsinhrathod031@gmail.com>
2026-01-05 11:16:56 +0530

    Add files via upload

---
+++ Samrathsinh D2.py
@@ -0,0 +1,23 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=("Hello world")
+print(x)
+Hello world
+x=15
+print(x)
+15
+x=True
+y=False
+print(x<y)
+False
+x=("Apple","Banana","Cherry")
+print(x)
+('Apple', 'Banana', 'Cherry')
+X=["Apple","Banana","Cherry"]
+print(x)
+('Apple', 'Banana', 'Cherry')
+X={"Name","John","Age","36"}
+print(x)
+('Apple', 'Banana', 'Cherry')
+print(X)
+{'Name', '36', 'Age', 'John'}
