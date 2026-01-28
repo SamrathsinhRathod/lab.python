25683418c0f0836382d0744e522918ea3419cb59
SamrathsinhRathod <Samrathsinhrathod031@gmail.com>
2026-01-28 08:30:51 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,52 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+p=float(input("Enter the principal Amount"))
+Enter the principal Amount5000
+r=float(input("Enter the rate of interest"))
+Enter the rate of interest9
+q=float(input("Enter the time(in years)"))
+Enter the time(in years)5
+si=(p*q*r)/100
+print("Simple interest is:",si)
+Simple interest is: 2250.0
+a=int(input("ENter First Number"))
+ENter First Number99
+b=int(input("Enter Seconf Number:"))
+Enter Seconf Number:55
+print("MAximum Number is :",max(a,b))
+MAximum Number is : 99
+for i in range (1,6):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+s=input("Enter A String:")
+Enter A String:HEllo World
+print("Length of the string is:",len(s)
+
+      )
+Length of the string is: 11
+print("Welcome")
+Welcome
+a=("Hello")
+print(a[1])
+e
+b=("python")
+print(b[5])
+n
+number = float(input("Enter a Number:"))
+Enter a Number:6
+print("positive" if number >=0 else "Negative")
+positive
+numbers=[10,20,30]
+numbers.append(40)
+print(numbers)
+[10, 20, 30, 40]
+name = input("Enter Your name:")
+Enter Your name:samrath
+print("Hello",name)
+Hello samrath
